dbbfe67dfa6dcf8e157746b5571000ceb578490f
Zaher Madi <zahermadi@yahoo.fr>
2025-06-26 01:23:20 +0200

    Formulaire 2.0.10 - Test no test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
+    <div className="App">
+      <h1>Test</h1>
+      </div>,
     <Router>
       <Routes>
         <Route path="/" element={<Formulaire />} />
